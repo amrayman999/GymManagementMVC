c1c3a3ac0fd43aaf65de25d076d1dec7e8335ea7
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-10-07 18:04:47 +0300

    Part07 - DbContext - Apply Configurations

---
+++ GymManagementDAL/Data/Contexts/GymDbContext.cs
@@ -0,0 +1,34 @@
+﻿
+using GymManagementDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GymManagementDAL.Data.Contexts
+{
+    public class GymDbContext : DbContext
+    {
+        public GymDbContext(DbContextOptions<GymDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        #region DbSets
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<HealthRecord> HealthRecords { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+
+
+
+        #endregion
+    }
+}
