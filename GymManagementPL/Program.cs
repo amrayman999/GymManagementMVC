664dadd4f7e8b423eae9cc4c9c75ef7c928b6697
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-10-17 10:28:57 +0300

    Part01 Controller - Action

--- GymManagementPL/Program.cs
+++ GymManagementPL/Program.cs
@@ -21,6 +21,7 @@ namespace GymManagementPL
             });
 
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<ISessionRepository, SessionRepository>();
             builder.Services.AddAutoMapper(x => x.AddProfile(new MappingProfile()));
             var app = builder.Build();
 
