ef609a9985dd37c16d0cbe0e7c79f30a913b13ff
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-10-21 13:36:57 +0300

    Part01 Member Controller - Index (Action & View)

---
+++ GymManagementPL/Controllers/MemberController.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly IMemberService _memberService;
+        public MemberController(IMemberService memberService)
+        {
+            _memberService = memberService;
+        }
+        public IActionResult Index()
+        {
+            var members = _memberService.GetAllMembers();
+            return View(members);
+        }
+    }
+}
