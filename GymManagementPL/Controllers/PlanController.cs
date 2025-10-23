edf900c078f1c38a6f21b98096b9f0633a957a8b
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-10-23 14:11:35 +0300

    Part01 PlanController - Index

---
+++ GymManagementPL/Controllers/PlanController.cs
@@ -0,0 +1,19 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly IPlanService _planService;
+        public PlanController(IPlanService planService)
+        {
+            _planService = planService;
+        }
+        public IActionResult Index()
+        {
+            var plans = _planService.GetAllPlans();
+            return View(plans);
+        }
+    }
+}
